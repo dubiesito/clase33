9b0605d118683820ab6241a64770a1bedda58433
dubiesito <manuehespinoza@gmail.com>
2025-08-19 17:40:19 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,19 @@
-// main.dart 👀 Build
 import 'package:flutter/material.dart';
-import 'package:recipebook/screens/home_screen.dart'; // Material app
+import 'view/counter_view.dart';
 
-// Run | Debug | Profile
-void main() => runApp(const MyApp()); // primera llamada de la app
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // StatelessWidget = clase dinámica
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // header wrap del container
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false, // widget constante
-      title: "Libro de las Plantas",
-      home: PlantsBook(), // MaterialApp
+    return MaterialApp(
+      title: 'Counter MVVM',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const CounterView(),
     );
-  }
-}
-
-class PlantsBook extends StatelessWidget {
-  const PlantsBook({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 4,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.amber.shade300,
-          title: const Text(
-            'Medicina Natural',
-            style: TextStyle(color: Color.fromARGB(255, 8, 8, 8)),
-          ), // Text
-          bottom: const TabBar(
-            indicatorColor: Colors.blue,
-            labelColor: Colors.white,
-            unselectedLabelColor: Colors.white,
-            tabs: [
-              Tab(
-                icon: Icon(Icons.home),
-                text: 'Home',
-              ), // Tab
-              Tab(
-                icon: Icon(Icons.question_mark),
-                text: 'Question',
-              ), // Tab
-              Tab(
-                icon: Icon(Icons.people),
-                text: 'People',
-              ), // Tab
-              Tab(
-                icon: Icon(Icons.info),
-                text: 'Info',
-              ), // Tab
-            ],
-          ), // TabBar
-        ), // AppBar
-        body: const TabBarView(
-          children: [
-            HomeScreen(),
-          ],
-        ), // Scaffold
-      ),
-    ); // DefaultTabController
   }
 }
